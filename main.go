cfc792c58f6404ebd6407059a542e1eda87ef330
kressin <luiz.kressin@gmail.com>
2024-05-12 20:11:38 -0300

    custom type declarations; receiver functions;

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	// it returns a new slice that we pass to the variable cards
 	cards = append(cards, "Six of Spades")
 
-	//range is a keyword that we use when we want to iterate over every element of a slice
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
