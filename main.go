62d8f5bd5d504f1c92426900df1a42e63cc7f55f
kressin <luiz.kressin@gmail.com>
2024-05-12 20:40:22 -0300

    creating a new deck;

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	// it returns a new slice that we pass to the variable cards
-	cards = append(cards, "Six of Spades")
-
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
